5e17072fa77c35a8ee517c1251ba197bc352141c
Hedi18 <studiawencyl@gmail.com>
2023-02-02 20:28:28 +0100

    dodanie Tablicy

---
+++ src/Tablice.java
@@ -0,0 +1,14 @@
+public class Tablice {
+    public static void main(String[] args) {
+        String[] imiona = new String[3];
+        imiona[0] = "Bartek";
+        imiona[1] = "Tomek";
+        imiona[2] = "Asia";
+
+        for(int i=0; i<imiona.length;i++){
+            System.out.println(imiona[i]);
+        }
+
+
+    }
+}
