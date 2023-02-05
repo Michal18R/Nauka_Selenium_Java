141ba2bb01b3e6afe0a83473b540792fc40fc446
Hedi18 <studiawencyl@gmail.com>
2023-02-05 21:16:19 +0100

    Zmiana w klasach Auto i AutoTest wykorzystując konstruktory

--- src/Auto.java
+++ src/Auto.java
@@ -12,10 +12,25 @@ public class Auto {
         System.out.println("Hamuj!");
     }
 
-    public void info(){
+    // poniżej dwa utworzone konstruktory bez wartości i z wartością
+    public Auto(){
+
+    }
+    public Auto (String marka, String model, int rok, int przebieg){
+        this.marka = marka;
+        this.model = model;
+        this.rok = rok;
+        this.przebieg = przebieg;
+
         System.out.println("Marka: " + marka);
         System.out.println("Model: " + model);
         System.out.println("Rok produkcji: " + rok);
         System.out.println("Przebieg: " + przebieg);
     }
+    /*public void info(){
+        System.out.println("Marka: " + marka);
+        System.out.println("Model: " + model);
+        System.out.println("Rok produkcji: " + rok);
+        System.out.println("Przebieg: " + przebieg);
+    }*/
 }
--- src/AutoTest.java
+++ src/AutoTest.java
@@ -1,25 +1,25 @@
 public class AutoTest {
     public static void main(String[] args) {
-
-        Auto mercedes = new Auto();
-        mercedes.marka = "Mercedes";
+        //użycie konstruktora w Auto do szybszego wywołania obiektu
+        Auto mercedes = new Auto("Mercedes", "Klasa S",2000, 1000 );
+        /*mercedes.marka = "Mercedes";
         mercedes.model = "Klasa S";
         mercedes.rok = 2000;
-        mercedes.przebieg= 1000;
+        mercedes.przebieg= 1000;*/
 
         mercedes.jedz();
-        mercedes.info();
+        //mercedes.info();
         mercedes.hamuj();
 
 
-        Auto audi = new Auto();
-        audi.marka = "Audi";
+        Auto audi = new Auto("Audi","A5", 2020, 100);
+        /*audi.marka = "Audi";
         audi.model = "A5";
         audi.rok = 2020;
-        audi.przebieg = 100;
+        audi.przebieg = 100;*/
 
         audi.jedz();
-        audi.info();
+        //audi.info();
         audi.hamuj();
 
     }
