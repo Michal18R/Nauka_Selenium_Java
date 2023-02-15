e7fc9109637ce6d37a0fd22805c5889936a68a42
Hedi18 <studiawencyl@gmail.com>
2023-02-15 19:56:31 +0100

    Użycie poliformizmu

--- src/drivers/WebDriversTest.java
+++ src/drivers/WebDriversTest.java
@@ -2,12 +2,25 @@ package drivers;
 
 public class WebDriversTest {
     public static void main(String[] args) {
-        ChromeDriver chrome = new ChromeDriver();
-        FirefoxDriver firefox = new FirefoxDriver();
+        //użycie poliformizmu
+        WebDriver driver = getDriver("chrome");
+        WebDriver driver2 = getDriver("firefox");
+        //FirefoxDriver firefox = new FirefoxDriver();
 
-        chrome.get();
-        chrome.findElementBy();
-        firefox.get();
-        firefox.findElementBy();
+        assert driver != null;
+        driver.get();
+        driver.findElementBy();
+        assert driver2 != null;
+        driver2.get();
+        driver2.findElementBy();
+    }
+
+    private static WebDriver getDriver(String name){
+        if(name.equals("chrome")){
+            return new ChromeDriver();
+        } else if (name.equals("firefox")){
+            return new FirefoxDriver();
+        }
+        return null;
     }
 }
