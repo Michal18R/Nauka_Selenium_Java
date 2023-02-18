ad9c127f745c14fb3ad4a455036e6f9783a211e9
Hedi18 <studiawencyl@gmail.com>
2023-02-18 21:18:42 +0100

    update i stworzenie klasy anonimowej WebDriverSecondTest

--- src/drivers/DriverType.java
+++ src/drivers/DriverType.java
@@ -1,7 +1,18 @@
 package drivers;
 
+import java.sql.Driver;
+
 public enum DriverType {
 
-    CHROME,
-    FIREFOX
+    CHROME("chrome", "/src/resources/chromedrive.exe"),
+    FIREFOX( "firefox", "/src/resources/firefox.exe");
+
+
+    String name;
+    String path;
+
+    DriverType(String name, String path){
+        this.name = name;
+        this.path = path;
+    }
 }
--- src/drivers/WebDriversTest.java
+++ src/drivers/WebDriversTest.java
@@ -3,6 +3,13 @@ package drivers;
 public class WebDriversTest {
     public static void main(String[] args) {
         //użycie poliformizmu
+        DriverType[] driverTypes = DriverType.values();
+        for (int i=0; i< driverTypes.length; i++){
+            System.out.println(driverTypes[i].name);
+            System.out.println(driverTypes[i].path);
+        }
+
+
         WebDriver driver = getDriver(DriverType.CHROME);
         WebDriver driver2 = getDriver(DriverType.FIREFOX);
         //FirefoxDriver firefox = new FirefoxDriver();
@@ -16,9 +23,12 @@ public class WebDriversTest {
     }
 
         private static WebDriver getDriver(DriverType type){
-            if(type==DriverType.CHROME){
+            if(type.name.equals("chrome")){
+                System.out.println(type.path);
                 return new ChromeDriver();
-            } else if (type==DriverType.FIREFOX){
+
+            } else if (type.name.equals("firefox")){
+                System.out.println(type.path);
                 return new FirefoxDriver();
             }
             return null;
