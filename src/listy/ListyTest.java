b88a729d4f3418564d19537bcefd9fe1fe0232eb
Hedi18 <studiawencyl@gmail.com>
2023-02-21 20:36:39 +0100

    dodanie klasy związanej z Listą i Zbiorem

---
+++ src/listy/ListyTest.java
@@ -0,0 +1,66 @@
+package listy;
+
+import lambda.ItStudent;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ListyTest {
+
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+        names.add("Paweł");
+        names.add("Kasia");
+        names.add("Ola");
+        names.add("Paweł");
+
+        // wypisuje 3 element listy names
+        System.out.println(names.get(2));
+        // pokazuje miejsce wyrażenia "Paweł" w liscie
+        System.out.println(names.indexOf("Paweł"));
+        System.out.println(names.lastIndexOf("Paweł"));
+        // sprawdza, czy w liscie jest "Ola"
+        System.out.println(names.contains("Ola"));
+        // sprawdza ile elemntów jest w liście
+        System.out.println(names.size());
+        System.out.println(names.isEmpty());
+
+        List<Integer> numbers = new LinkedList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+
+        System.out.println("Sprawdzamy drugi typ Listy");
+
+        // wypisuje 3 element listy names
+        System.out.println(numbers.get(2));
+        // pokazuje miejsce wyrażenia "Paweł" w liscie
+        System.out.println(numbers.indexOf(3));
+        System.out.println(numbers.lastIndexOf(1));
+        // sprawdza, czy w liscie jest "Ola"
+        System.out.println(numbers.contains("Ola"));
+        // sprawdza ile elemntów jest w liście
+        System.out.println(numbers.size());
+        System.out.println(numbers.isEmpty());
+
+        System.out.println("Kolejne zastosowania Listy");
+
+        List<String> diffNames = new ArrayList<>();
+        diffNames.addAll(names);
+
+        List<ItStudent> itStudent = new ArrayList<>();
+        itStudent.add(new ItStudent());
+
+        for(int i=0; i<names.size();i++){
+            System.out.println(names.get(i));
+        }
+        // inne wypisanie elementów listy
+        for(String name : names){
+            System.out.println(name);
+        }
+
+
+    }
+}
